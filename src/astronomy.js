b411153be7144f6c5f94b5ae1a48d35287120225
jakechaley <jakechaley@gmail.com>
2022-02-09 14:49:41 -0800

    get application to functional state
    Co-authored-by: William Jameson <williamjameson90@gmail.com>

---
+++ src/astronomy.js
@@ -0,0 +1,17 @@
+export default class Astronomy {
+  static randomImage(imageNumber) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&count=${imageNumber}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import *** from './js/*** */.js';
+import Astronomy from "./astronomy.js";
+
+// function getRandomElements(body) {
+//   for (let i = 0; i <= body.length; i++) {
+//     $(".randomResult").append(`<img src= "${body[i].url}" alt="space image">`);
+//   }
+// }
+
+
+
+$(document).ready(function() {
+  $("#randomImage").click(function() {
+    const imageNumber = $('#numberOfImages').val();
+    $('#numberOfImages').val("");
+    let promise = Astronomy.randomImage(imageNumber);
+    promise.then(function (response) {
+      const body = JSON.parse(response);
+      $('.randomResult').text('');
+      for (let i=0; i < body.length; i++) {
+        // console.log(body[i].url);
+        $(".randomResult").append(`${body[i].title} <img src="${body[i].url}" alt="space image"> <br> <br> <br> `);
+      }
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
