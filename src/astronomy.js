a95855dd04db6bbf460cb784615cde69d010140e
jakechaley <jakechaley@gmail.com>
2022-02-10 16:12:23 -0800

    add second API call using async function
    Co-authored-by: William Jameson <williamjameson90@gmail.com>

--- src/astronomy.js
+++ src/astronomy.js
@@ -14,4 +14,17 @@ export default class Astronomy {
       request.send();
     });
   }
+
+  static async getRoverImage(solDays) {
+    try {
+      const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solDays}&api_key=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      console.log(response);
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,32 @@ import './css/styles.css';
 import Astronomy from "./astronomy.js";
 
 // function getRandomElements(body) {
-//   for (let i = 0; i <= body.length; i++) {
+//   for (let i = 0; i < body.length; i++) {
 //     $(".randomResult").append(`<img src= "${body[i].url}" alt="space image">`);
 //   }
 // }
 
+function getElements(response) {
+  if (response.photos.length > 0) {
+    console.log(response.photos);
+    $('.roverImages').append(`<div class="card mb-3"> <img class="card-img-top" src="${response.photos[0].img_src}"/>`);
+  } else if (response.photos.length <= 0) {
+    $('.showErrors').text(`There were no photots taken that day!`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function roverApiCall(solDays) {
+  const response = await Astronomy.getRoverImage(solDays);
+  console.log(response);
+  getElements(response);
+}
 
 
 $(document).ready(function() {
-  $("#randomImage").click(function() {
+  $("#randomImageInput").submit(function(event) {
+    event.preventDefault();
     const imageNumber = $('#numberOfImages').val();
     $('#numberOfImages').val("");
     let promise = Astronomy.randomImage(imageNumber);
@@ -21,11 +38,26 @@ $(document).ready(function() {
       const body = JSON.parse(response);
       $('.randomResult').text('');
       for (let i=0; i < body.length; i++) {
-        // console.log(body[i].url);
-        $(".randomResult").append(`${body[i].title} <img src="${body[i].url}" alt="space image"> <br> <br> <br> `);
+        $(".randomResult").append(`<div class="card mb-3"> <img class="card-img-top" src="${body[i].url}" alt="space image"> <div class="card-body"> <p class="card-title title"> <strong> ${body[i].title}</strong> </p>  <p class="card-text explanation">${body[i].explanation}</p>
+        </div>
+        </div>`);
+      
       }
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
+    })
+      .catch(function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error}`);
+      });
+
+
+  });
+
+  $("#marsImageInput").submit(function(event) {
+    event.preventDefault();
+    $(".roverHeader").show();
+    const solDays = $('#solDays').val();
+    $('#solDays').val("");
+    $(".roverImages").text('');
+    console.log(solDays);
+    roverApiCall(solDays);
   });
 });
